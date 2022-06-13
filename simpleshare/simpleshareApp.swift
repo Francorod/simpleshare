0b62abefd72520623c5c5cce8507511fb532a077
Franco Rod <francorodios@gmail.com>
2022-06-13 17:53:43 -0400

    Initial Commit

---
+++ simpleshare/simpleshareApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleshareApp.swift
+//  simpleshare
+//
+//  Created by Franco Rodrigues on 6/13/22.
+//
+
+import SwiftUI
+
+@main
+struct simpleshareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
