6f6d6e9440fe4621c273ee086adc255083b12737
Franco Rod <francorodios@gmail.com>
2022-06-13 18:14:05 -0400

    update

--- simpleshare/ContentView.swift
+++ simpleshare/ContentView.swift
@@ -9,8 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button(action: actionSheet) {
+            Image(systemName: "square.and.arrow.up")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 36, height: 36)
+        }
+    }
+    func actionSheet() {
+        guard let data = URL(string: "https://www.zoho.com") else { return }
+        let av = UIActivityViewController(activityItems: [data], applicationActivities: nil)
+        UIApplication.shared.windows.first?.rootViewController?.present(av, animated: true, completion: nil)
     }
 }
 
